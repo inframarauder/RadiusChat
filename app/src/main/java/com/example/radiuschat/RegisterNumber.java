1ef3c74011a9efa5c0403a32b34d0bf8ec92c8ca
subhasis020299 <subhasisdas125@gmail.com>
2021-06-06 15:47:05 +0530

    firebase user registration implemented

--- app/src/main/java/com/example/radiuschat/RegisterNumber.java
+++ app/src/main/java/com/example/radiuschat/RegisterNumber.java
@@ -1,5 +1,6 @@
 package com.example.radiuschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -10,8 +11,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.radiuschat.utils.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterNumber extends AppCompatActivity {
 
@@ -25,13 +31,33 @@ public class RegisterNumber extends AppCompatActivity {
     public void handleRegistrationClick(View v){
         String phoneNumber =((EditText)findViewById(R.id.editTextPhone)).getText().toString();
 
-        //save to firebase...
+        //create firebase reference...
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference root = db.getReference();
-        root.setValue(phoneNumber);
+        DatabaseReference userRef = db.getReference("users");
+
+        //query and check if user already exists...
+        Query checkUser = userRef.orderByChild("phoneNumber").equalTo(phoneNumber);
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(!snapshot.exists()){
+                    //if user does not exist, create and insert new user record...
+                    userRef.push().setValue(new User(phoneNumber));
+                }
+                //login user by writing user data to shared preference...
+                loginUser(phoneNumber);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+                Log.d("onCancelled",error.getDetails());
+            }
+        });
+    }
 
 
-        //after success, save to shared preference:
+    private void loginUser(String phoneNumber){
+        //writing user to shared preference .....
         SharedPreferences pref = getSharedPreferences("com.example.radiuschat.users", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         editor.putString("phoneNumber",phoneNumber);
